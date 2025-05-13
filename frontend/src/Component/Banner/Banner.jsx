6bdc7fa0926ab4d2e72149fb6563df18e6b5facf
kabir-dharshaaan-tech <dharshaankabir@gmail.com>
2025-05-13 14:26:35 +0530

    done

--- frontend/src/Component/Banner/Banner.jsx
+++ frontend/src/Component/Banner/Banner.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+
+import React from 'react';
+import bannerImg from '../../assets/banner.png';
 
 function Banner() {
   return (
-    <div><p>Banner</p></div>
-  )
+    <div className="w-full overflow-hidden">
+      <img
+        src={bannerImg}
+        alt="Banner"
+        className="w-full h-auto"
+        style={{ display: 'block' }}
+      />
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
